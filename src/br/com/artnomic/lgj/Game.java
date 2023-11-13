a0d7564487acdf58bf3734301abcd83113cc114f
arthur.berson <arthur.berson@idwall.co>
2023-11-13 15:38:41 -0300

    feat: a new block-game

--- src/br/com/artnomic/lgj/Game.java
+++ src/br/com/artnomic/lgj/Game.java
@@ -4,20 +4,37 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class Game extends JFrame {
-    //TODO: Meu checkpoint é a imagem 1.4 - Movendo objetos pela tela
+
+    private final int FPS = 1000 / 20;
+
+    class Element {
+        private int x, y, width, height;
+        public float velocity;
+
+        public Element(int x, int y, int width, int height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
 
     private JPanel canva;
-    private int px, py;
-    private Point mouseClick = new Point();
-    private int FPS = 1000 / 20;
     private boolean playing = true;
+    private boolean gameOver = false;
+
+    private Element shot, player;
+    private Element[] blocks;
+
+
+    private int points;
+    private int larg = 50;
+    private int limitLine = 350;
+    private java.util.Random r = new java.util.Random();
 
     private boolean[] controllArrowKey = new boolean[4];
-
 
     public Game() {
         this.addKeyListener(new KeyListener() {
@@ -36,7 +53,21 @@ public class Game extends JFrame {
             }
         });
 
+        shot = new Element(0, 0, 1, 0);
+        player = new Element(0, 0, larg, larg);
+        player.velocity = 5;
+
+        blocks = new Element[5];
+        for (int i = 0; i < blocks.length; i++) {
+            int space = i * larg + 10 * (i + 1);
+            blocks[i] = new Element(space, 0, larg, larg);
+            blocks[i].velocity = 1;
+        }
+
         canva = new JPanel() {
+
+            private static final long serialVersionUID = 1L;
+
             @Override
             public void paintComponent(Graphics g) {
                 //Exemplo de como fazer cor:
@@ -44,48 +75,43 @@ public class Game extends JFrame {
 
                 //Limpar todos os desenhos
                 g.setColor(Color.WHITE);
-                g.fillRect(0, 0, canva.getWidth(), canva.getHeight());
+                g.fillRect(0, 0,
+                        canva.getWidth(), canva.getHeight());
 
-                int x = canva.getWidth() / 2 - 20 + px;
-                int y = canva.getHeight() / 2 - 20 + py;
+                g.setColor(Color.RED);
+                g.fillRect(shot.x, shot.y,
+                        shot.width, canva.getHeight());
 
-                g.setColor(Color.PINK);
-                g.fillRect(x, y, 40, 40);
-                g.drawString("Agora eu estou em " + x + " x " + y, 5, 10);
+                g.setColor(Color.GREEN);
+                g.fillRect(player.x, player.y,
+                        player.width, player.height);
+
+                g.setColor(Color.BLUE);
+                for (Element block : blocks) {
+                    g.fillRect(block.x, block.y,
+                            block.width, block.height);
+                }
+
+                g.setColor(Color.GRAY);
+                g.drawLine(0, limitLine,
+                        canva.getWidth(), limitLine);
+
+                g.drawString("Pontos: " + points, 0, 10);
+
             }
         };
 
-        canva.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                mouseClick = e.getPoint();
-            }
+        getContentPane().add(canva);
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        super.getContentPane().add(canva);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
         setSize(640, 480);
         setVisible(true);
+
+        player.x = canva.getWidth() / 2 - player.width /2;
+        player.y = canva.getHeight() - player.height;
+
+        shot.height = canva.getHeight() - player.height;
     }
 
     public void start() {
@@ -101,25 +127,58 @@ public class Game extends JFrame {
     }
 
     private void updateGame() {
-        if (controllArrowKey[0]) {
-            py--;
-        } else if (controllArrowKey[1]) {
-            py++;
+        if(gameOver) {
+            return;
         }
 
         if (controllArrowKey[2]) {
-            px--;
+            player.x -= player.velocity;
         } else if (controllArrowKey[3]) {
-            px++;
+            player.x += player.velocity;
         }
 
-//        px = mouseClick.x;
-//        py = mouseClick.y;
+        if(player.x < 0) {
+            player.x = canva.getWidth() - player.width;
+        }
+
+        if(player.x + player.width > canva.getWidth()) {
+            player.x = 0;
+        }
+
+        shot.y = 0;
+        shot.x = player.x + player.width / 2;
+
+        for (Element block : blocks) {
+            if (block.y > limitLine) {
+                gameOver = true;
+                break;
+            }
+
+            if (collide (block, shot) && block.y > 0) {
+                block.y -= block.velocity * 2;
+                shot.y = block.y;
+            } else {
+                int fate = r.nextInt(10);
+
+                if(fate == 0) {
+                    block.y += block.velocity + 1;
+                } else if (fate == 5) {
+                    block.y -= block.velocity;
+                } else {
+                    block.y += block.velocity;
+                }
+            }
+            points = points + blocks.length;
+        }
     }
 
-//    private boolean colide(Element a, Element b) {
-//
-//    }
+    private boolean collide(Element a, Element b) {
+        if (a.x + a.width >= b.x && a.x <= b.x + b.width) {
+            return true;
+        }
+
+        return false;
+    }
 
     private void arrowKey(int key, boolean press) {
         switch (key) {
