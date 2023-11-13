cc310e33cb06f5f2b94cc9b31aaff6ed9663b72c
arthur.berson <arthur.berson@idwall.co>
2023-11-13 17:55:54 -0300

    feat: the beginning of the project class division, add the class util.

--- src/br/com/artnomic/lgj/base/Util.java
+++ src/br/com/artnomic/lgj/base/Util.java
@@ -1,4 +1,19 @@
 package br.com.artnomic.lgj.base;
 
 public class Util {
+    public static boolean collide(Element a, Element b) {
+        if (!a.isActive() || !b.isActive())
+            return false;
+
+        final int plA = a.getPx() + a.getWidth();
+        final int plB = b.getPx() + b.getWidth();
+        final int paA = a.getPy() + a.getHeight();
+        final int paB = b.getPy() + b.getHeight();
+
+        if (plA > b.getPx() && a.getPx() < plB && paA > b.getPy() && a.getPy() < paB) {
+            return true;
+        }
+
+        return false;
+    }
 }
